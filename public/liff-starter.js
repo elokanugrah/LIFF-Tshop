c709b99ac5c12db5f34ed577a376502fccb1ffef
elokanugrah <33139266+elokanugrah@users.noreply.github.com>
2020-01-05 22:30:04 +0700

    change to node

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1653657030-Kk24lV06";   // change the default LIFF value if you are not using a node server
+    const useNodeJS = true;   // if you are not using a node server, set this value to false
+    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
  
     // DO NOT CHANGE THIS
     let myLiffId = "";
